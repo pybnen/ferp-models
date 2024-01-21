b7f22dc06aa98d0367b88a5edc677f486fd20260
pybnen <ha.reisinger@gmail.com>
2024-01-21 14:08:18 +0100

    Add logging and time pipeline

--- code/pipeline.py
+++ code/pipeline.py
@@ -2,29 +2,52 @@
 from datetime import datetime
 import sys, os, shutil, errno, subprocess, signal
 import time
+from argparse import ArgumentParser
+import uuid
 
 home = os.path.dirname(os.path.abspath(__file__)) + "/"
-# dependencies = ["ijtihad/ijtihad", "picosat-965/picosat", 
-#                 "booleforce-1.2/tracecheck", "toferp/toferp", 
-#                 "ferpcert/ferpcheck", "ferpcert2/ferpcert",
-#                 "certcheck-1.0.1/certcheck"]
-
 #TODO deploy my version of ijtihad in repo, use this repo in requirements.py
 # use my version of ijtihad
-dependencies = [
-  "../../ijtihad/ijtihad",
-  "picosat-965/picosat", 
-  "booleforce-1.2/tracecheck",
-  "../../toferp/build/toferp", 
-  "../../ferpcert/build/ferpcheck",
-  "ferpcert2/ferpcert",       # ignore
-  "certcheck-1.0.1/certcheck" # ignore
-]
+QBFSOLVER = f"{home}../../ijtihad/ijtihad"
+SATSOLVER = f"{home}picosat-965/picosat"
+TRACECHECK = f"{home}booleforce-1.2/tracecheck"
+TOFERP = f"{home}../../toferp/build/toferp"
+FERPCHECK = f"{home}../../ferpcert/build/ferpcheck"
+FERPCERT = f"{home}ferpcert2/ferpcert"
+CERTCHECK = f"{home}certcheck-1.0.1/certcheck"
+dependencies = [QBFSOLVER, SATSOLVER, TRACECHECK, TOFERP,  FERPCHECK, FERPCERT, CERTCHECK]
 
-dependencies = [home + x for x in dependencies]
-tmp_dir = home + "tmp/{}/".format(datetime.now().strftime("%Y%m%d_%H-%M-%S"))
+run_name = "run_" + str(uuid.uuid1()) # datetime.now().strftime("%Y%m%d_%H%M%S")
+log_file = None
+keep_dir = None
+keep_ferp_dir = None
+tmp_dir = home + "tmp/{}/".format(run_name)
 
-trim = False
+
+def copy_file(src, name):
+  global keep_dir
+  if keep_dir:
+    shutil.copyfile(src, keep_dir + "/" + name)
+
+
+def copy_ferp(src, name):
+  global keep_ferp_dir
+  if keep_ferp_dir:
+    shutil.copyfile(src, keep_ferp_dir + "/" + name)
+
+
+def get_log_fp():
+  global log_file
+  return open(log_file, 'a') if log_file is not None else open(os.devnull, 'w')
+
+
+def log(msg="", prefix="*** ferpmodels: "):
+  global log_file
+  if log_file is None:
+    return
+  with open(log_file, 'a') as fp:
+    fp.write(prefix + msg + "\n")
+  
 
 def assure_dir(path):
   try:
@@ -36,41 +59,51 @@ def assure_dir(path):
 
 
 def parse_args():
-  sys.stdout.write("Parsing command line arguments ... ")
-  assert len(sys.argv) == 2 or len(sys.argv) == 3
-
-  input_path = os.path.abspath(sys.argv[1])
+  global log_file, run_name, keep_dir, keep_ferp_dir
+  
+  parser = ArgumentParser()
+  parser.add_argument("-K", "--Keep", help="keep all intermediate files in <dir>", metavar="<dir>")
+  parser.add_argument("-k", "--keep", help="keep ferp proof in <dir>", metavar="<dir>")
+  parser.add_argument("-l", "--log", help="Log all tool output to <dir>", metavar="<dir>")
+  parser.add_argument("qdimacs_file", help="QBF formular in qdimacs file")
+  parser.add_argument("output_file", help="Certificat file (unsat only)", default=None, nargs="?")  
+  args = parser.parse_args()
+  
+  input_path = os.path.abspath(args.qdimacs_file)
   qbf_name = ".".join(input_path.split("/")[-1].split(".")[:-1])
   if not os.path.exists(input_path):
     raise OSError("Input file %s does not exist" % input_path)
   
+  if args.Keep is not None:
+    keep_dir = os.path.abspath(args.Keep)
+    assure_dir(keep_dir)
+    
+  if args.keep is not None:
+    keep_ferp_dir = os.path.abspath(args.keep)
+    assure_dir(keep_ferp_dir)
+  
+  if args.log is not None:
+    log_path = os.path.abspath(args.log)
+    assure_dir(log_path)
+    log_file = log_path + "/{}.log".format(run_name)
+  
   output_path = None
-  if (len(sys.argv) == 3):
-    output_path = os.path.abspath(sys.argv[2])
+  if args.output_file is not None:
+    output_path = os.path.abspath(args.output_file)
     output_dir = "/".join(output_path.split("/")[:-1])
     assure_dir(output_dir)
-  
-  print("DONE")
+
   return input_path, output_path, qbf_name
 
 
 def check_dependencies():
-  padlen = len(max(dependencies, key=len))
-  missing = 0
   for dep in dependencies:
-    sys.stdout.write("Checking dependency: %s " % dep)
-    sys.stdout.write((" " * (padlen - len(dep))) + "... ")
-    if os.path.exists(dep) :
-      print("OK")
-    else:
-      print("MISSING")
-      missing += 1
-  if missing > 0:
-    raise OSError("One or more dependencies are missing")
+    if not os.path.exists(dep) :
+      raise OSError("One or more dependencies are missing")
 
 
 def clean(ret):
-  # shutil.rmtree(tmp_dir, ignore_errors=True)
+  shutil.rmtree(tmp_dir, ignore_errors=True)
   sys.exit(ret)
 
 
@@ -80,33 +113,43 @@ def term_handler(signum, frame):
 
 
 def main():
+  global run_name
+  
   signal.signal(signal.SIGTERM, term_handler)
   signal.signal(signal.SIGINT, term_handler)
 
-  print("Home is at", home)
-  print("Tmp is at", tmp_dir)
-
   input_path, output_path, qbf_name = parse_args()
+  
   check_dependencies()
   assure_dir(tmp_dir)
   FNULL = open(os.devnull, 'w')
-
+  
+  log(f"run on {input_path}")
+  
   # Call the qbf solver and produce the .cnf file
-
+  log()
+  log("running QBF solver")
+  log()
+  
   sys.stdout.write("Calling QBF solver ... ")
   sys.stdout.flush()
+  
+  log_fp = get_log_fp()
   start_time = time.time()
   ret = subprocess.call([
-                          dependencies[0],
+                          QBFSOLVER,
                           "--wit_per_call=-1",
                           "--cex_per_call=-1",
                           "--tmp_dir="+tmp_dir,
                           "--log_phi="+tmp_dir+"tmp.cnf",
                           "--log_ksi="+tmp_dir+"tmp.cnf", # logging in case of SAT
                           input_path
-                        ]) #, stdout=subprocess.DEVNULL)
+                        ], stdout=log_fp)
   time_ijtihad = time.time() - start_time
+  log_fp.close()
   is_sat = False
+  
+  copy_file(tmp_dir + "tmp.cnf", f"{run_name}.cnf")
   
   if ret == 10:
     is_sat = True
@@ -126,14 +169,26 @@ def main():
     if len(f.readline()) == 0:
       print("Output of Ijtihad is empty, make sure that the given qbf is not trivially true or false, e.g. it contains only existential variables.")
       clean(100)
+
+  log()
+  log("QBF solver OK")
   
   # Call the sat solver again on the .cnf file
   # This has to be done because no proof logging can be done in incremental mode
+  log()
+  log("running SAT solver")
+  log()
+  
   start_gen = time.time()
 
   sys.stdout.write("Calling SAT solver ... ")
   sys.stdout.flush()
-  ret = subprocess.call([dependencies[1], "-v", "-T", tmp_dir+"tmp.proof", tmp_dir+"tmp.cnf"]) #, stdout=subprocess.DEVNULL)
+  
+  log_fp = get_log_fp()
+  ret = subprocess.call([SATSOLVER, "-v", "-T", tmp_dir+"tmp.proof", tmp_dir+"tmp.cnf"], stdout=log_fp) #, stdout=subprocess.DEVNULL)
+  log_fp.close()
+  
+  copy_file(tmp_dir + "tmp.proof", f"{run_name}.proof")
   
   if ret == 10:
     print("FAILED")
@@ -148,20 +203,26 @@ def main():
 
   assert (os.path.exists(tmp_dir + "tmp.proof"))
 
+  log()
+  log("SAT solver OK")
+  
   # The SAT solver only produces normal extended tracecheck proofs
   # Check the proof with tracecheck and extract binary resolution proof
-
+  log()
+  log("running tracecheck")
+  log()
+  
   sys.stdout.write("Checking %s proof ... " % ("sat" if is_sat else "unsat")) 
   sys.stdout.flush()
-  ret = subprocess.check_output([dependencies[2], "-v", "-B", tmp_dir + "tmp.proof2", "-c", tmp_dir + "tmp.cnf", tmp_dir + "tmp.proof"])
+  ret = subprocess.check_output([TRACECHECK, "-v", "-B", tmp_dir + "tmp.proof2", "-c", tmp_dir + "tmp.cnf", tmp_dir + "tmp.proof"])
   ret = ret.strip().decode('UTF-8')
   ret_lines = ret.split("\n")
+  log(ret, "")
   
-  print(ret)
+  copy_file(tmp_dir + "tmp.proof2", f"{run_name}.proof2")
   
   # if (ret == "resolved 1 root and 1 empty clause") or (ret == "resolved 0 roots and 1 empty clause" and is_sat):
   if ("resolved 1 root and 1 empty clause" in ret_lines or ("resolved 0 roots and 1 empty clause" in ret_lines and is_sat)):
-    if trim: os.remove(tmp_dir + "tmp.proof")
     print("DONE")
   else:
     print("FAILED")
@@ -170,14 +231,25 @@ def main():
     
   assert (os.path.exists(tmp_dir + "tmp.proof2"))
   
+  log()
+  log("tracecheck OK")
+      
   # Merge the comment information from the cnf and the binary resolution
   # proof into a FERP trace.
-
+  log()
+  log("producing FERP trace")
+  log()
+  
   sys.stdout.write("Producing FERP trace ... ")
   sys.stdout.flush()
-  start_toferp = time.time() 
-  ret = subprocess.call([dependencies[3], tmp_dir + "tmp.cnf", tmp_dir + "tmp.proof2", tmp_dir + "tmp.ferp"]) #, stdout=subprocess.DEVNULL)
+  log_fp = get_log_fp()
+  start_toferp = time.time()
+  ret = subprocess.call([TOFERP, tmp_dir + "tmp.cnf", tmp_dir + "tmp.proof2", tmp_dir + "tmp.ferp"], stdout=log_fp)
   end_toferp = time.time()
+  log_fp.close()
+  
+  copy_file(tmp_dir + "tmp.ferp", f"{run_name}.ferp")
+  copy_ferp(tmp_dir + "tmp.ferp", f"{run_name}.ferp")
   
   time_toferp = end_toferp - start_toferp
   time_gen = end_toferp - start_gen
@@ -186,46 +258,64 @@ def main():
     print("FAILED", ret)
     clean(6)
   else:
-    if trim: os.remove(tmp_dir + "tmp.cnf")
-    if trim: os.remove(tmp_dir + "tmp.proof2")
     print("DONE")
+    
+  log()
+  log("FERP trace OK")
 
   # Check whether the FERP trace is consistent
-
-  sys.stdout.write("Checking FERP trace ...\n")
+  log()
+  log("checking FERP trace")
+  log()
+  
+  sys.stdout.write("Checking FERP trace ...")
   sys.stdout.flush()
+  
+  log_fp = get_log_fp()
   start_time = time.time()
-  ret = subprocess.call([dependencies[4], input_path, tmp_dir + "tmp.ferp"]) #, stdout=subprocess.DEVNULL)
+  ret = subprocess.call([FERPCHECK, input_path, tmp_dir + "tmp.ferp"], stdout=log_fp)
   time_ferpcheck = time.time() - start_time
+  log_fp.close()
+  
   if ret != 0:
-    print("Checking FERP trace ... FAILED", ret)
+    print("FAILED", ret)
     clean(7)
   else:
-    print("Checking FERP trace ... DONE")
+    print("DONE")
     
+  log()
+  log("FERP trace valid")
+  
+  log()
+  log("PYTHON-TIMES")
+  log()
+  log(f"Solver:      {time_ijtihad:.6f} s", "")
+  log(f"Generation:  {time_gen:.6f} s", "")
+  log(f"ToFerp:      {time_toferp:.6f} s", "")
+  log(f"FerpCheck:   {time_ferpcheck:.6f} s", "")
+  log()
+  log("PYTHON-TIMES")
+
+  print("PYTHON-TIMES------------------------------")
+  print(f"Solver:      {time_ijtihad:.6f} s")
+  print(f"Generation:  {time_gen:.6f} s")
+  print(f"ToFerp:      {time_toferp:.6f} s")
+  print(f"FerpCheck:   {time_ferpcheck:.6f} s")
+  print("------------------------------------------")
+  
   if output_path is None or is_sat:
-    # ignore the rest for now
-    print("PYTHON-TIMES------------------------------")
-    print(f"Solver:      {time_ijtihad:.6f} s")
-    print(f"Generation:  {time_gen:.6f} s")
-    print(f"ToFerp:      {time_toferp:.6f} s")
-    print(f"FerpCheck:   {time_ferpcheck:.6f} s")
-    print("------------------------------------------")
-          
-    #printf("      /Check time in seconds: %.4f / %.4f" % (time_ijtihad, time_ferpcheck))
     clean(0)
   
   # Extract a circuit for the universals into an AIGER file
 
   sys.stdout.write("Extracting strategy ... ")
   sys.stdout.flush()
-  ret = subprocess.call([dependencies[5], input_path, tmp_dir + "tmp.ferp", output_path])
+  ret = subprocess.call([FERPCERT, input_path, tmp_dir + "tmp.ferp", output_path])
 
   if ret != 0:
     print("FAILED", ret)
     clean(8)
   else:
-    if trim: os.remove(tmp_dir + "tmp.ferp")
     print("DONE")
 
   # Merge AIGER and QDIMACS files into a formula checkable by a SAT solver
@@ -234,8 +324,10 @@ def main():
   sys.stdout.flush()
 
   FCNF = open(tmp_dir + "tmp.cnf2", "wb")
-  ret = subprocess.call([dependencies[6], input_path, output_path], stdout=FCNF)
+  ret = subprocess.call([CERTCHECK, input_path, output_path], stdout=FCNF)
   FCNF.close()
+  
+  copy_file(tmp_dir + "tmp.cnf2", f"{run_name}.cnf2")
   
   if ret != 0:
     print("FAILED", ret)
@@ -245,7 +337,7 @@ def main():
 
   sys.stdout.write("Check validity of certificate ... ")
   sys.stdout.flush()
-  ret = subprocess.call([dependencies[1], tmp_dir + "tmp.cnf2"],
+  ret = subprocess.call([SATSOLVER, tmp_dir + "tmp.cnf2"],
                         stdout=FNULL, stderr=FNULL)
   if ret == 10:
     print("FAILED")
